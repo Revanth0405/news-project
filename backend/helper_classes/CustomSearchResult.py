26c08d4ccbc4bd3be15767ae02157fcb130f8616
Krishna Modepalli <90754999+krishnamodepalli@users.noreply.github.com>
2025-01-07 23:33:03 +0530

    feat(search_api): Get link and title from every search result
    what's changed:
    - Typing the result from custom search, with a new class
      `CustomSearchResult`.
    - Getting the link from each of the search result in custom search.

---
+++ backend/helper_classes/CustomSearchResult.py
@@ -0,0 +1,20 @@
+"""
+This file contains the CustomSearchResult class, which is a class that
+represents a single search result from the Google Custom Search API.
+"""
+
+
+class CustomSearchResult:
+    """
+    CustomSearchResult class is a class that represents a single search result
+    from the Google Custom Search API.
+    """
+
+    def __init__(self, title: str, htmlTitle: str, link: str, displayLink: str):
+        self.title = title
+        self.htmlTitle = htmlTitle
+        self.link = link
+        self.displayLink = displayLink
+
+    def __str__(self):
+        return f"{self.title} - {self.displayLink}"
